e6ff6b4c19d4d9a3066efd6cd7cb057d06a7af14
eliot-gsd <94835940+eliot-gsd@users.noreply.github.com>
2022-11-29 10:00:01 +0100

    change

--- serveur.py
+++ serveur.py
@@ -1,65 +1,58 @@
 import socket
-import sys 
+import sys
+import os
+import pyaudio
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 3
+  
+audio = pyaudio.PyAudio()
 
 class ServiceEcoute:
+
     def __init__(self, port_serveur: int)-> None:
-        self.__socket_ecoute : socket = None
-        self.__port_ecoute : int = port_serveur
-        self.__socket_ecoute = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__socket_ecoute.bind(("",self.__port_ecoute))
-        # mise en ecoute avec le nombre de connexion simultanées possibles
-        self.__socket_ecoute.listen(1)
-        print(f"serveur en ecoute sur le port {self.__port_ecoute} ...")
+        self.__port_serveur = port_serveur
+        self.socket_ecoute: socket
 
+    def attente(self)-> socket:
+        self.socket_ecoute = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket_ecoute.bind(("", self.__port_serveur))
+        self.socket_ecoute.listen(1)
+        print(f"serveur en ecoute sur le port {self.__port_serveur} ...")
 
-
-    def attente(self)-> socket: 
-
-        # attente d'une demande de connexion
-        socket_echange, ADDR = self.__socket_ecoute.accept() # bloquante
-        # fermeture de l'ecoute (ici gestion d'un seul client)
+        socket_echange, ADDR = self.socket_ecoute.accept()
         print("une nouvelle demande de connexion")
-        # affichage des information de la machine distante
         print(f"sochet echange : {socket_echange}")
-        print(f"adr : {ADDR}")
+        #print(f"adr : {ADDR}")
         return socket_echange
 
 
-
 class ServiceEchange:
-    def __init__(self, socket_echange: socket)-> None: 
-        self.__socket_echange : socket = socket_echange
-
+    def __init__(self, socket_echange: socket)-> None:
+        self.__socket_echange = socket_echange
 
     def envoyer(self, msg: str)-> None:
         tab_bytes = msg.encode("utf-8")
         self.__socket_echange.send(tab_bytes)
 
-    def recevoir(self)-> str: 
-        tab_bytes = self.__socket_echange.recv(255)
-        res = tab_bytes.decode("utf-8")
-        return res 
+    def recevoir(self)-> str:
+        frames = self.__socket_echange.recv(1024)
+        return frames
+
 
     def echange(self)->None:
-        fin : bool = False
+        fin: bool = False
         while(not fin):
-            # mise en forme et affichage du message du client
             recu = self.recevoir()
-            # preparation de la réponse
             if recu == "fin":
                 fin = True
-            print(f"C => S: {recu}")
-            # envoi de la réponse
-            msg_server = f"[{recu}]"
-            self.envoyer({msg_server})
+            print(f"C => S : {recu}")
 
-    
-
-    def arret(self)-> None:
-        print(f"Connexion fermée")
-        self.__socket_echange.close()
-
-
+            # preparation de la réponse
+            msg_serveur = f"[{recu}]"
+            self.envoyer(msg_serveur)
 
 if __name__ == "__main__":
     # declaration des variables
@@ -71,10 +64,14 @@ if __name__ == "__main__":
     if len(sys.argv) >= 2:
         port_ecoute = int(sys.argv[1])
     else:
-        port_ecoute = 5000
-    
+        port_ecoute = 5001
+    #try:
     service_ecoute = ServiceEcoute(port_ecoute)
     socket_client = service_ecoute.attente()
     service_echange = ServiceEchange(socket_client)
-    service_echange.echange()
-    
+    frames = service_echange.recevoir()
+    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
+    for data in frames:
+        stream.write(data)
+    #except Exception as err:
+        #print("erreur : " + str(err))
