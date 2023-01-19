5f16bdc3bc7091bf08dff822bd20668d24487687
eliot-gsd <94835940+eliot-gsd@users.noreply.github.com>
2023-01-19 10:17:04 +0100

    tqrt

--- IHM.py
+++ IHM.py
@@ -173,37 +173,46 @@ def appel(ip):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ip,6000))
 
+
+
+
+
     thread_ecoute = Thread(target=receive_data)
     thread_enregistrer = Thread(target=send_data)
-
-
     p = pyaudio.PyAudio()
     connexion = True
     print("Le chat vocal va commencer")
-
     global receive_stream
     global send_stream
+    
     receive_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
     send_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
-
-
     if connexion == True:
+        thread_ecoute.start()
+    
+    elif connexion == True and connects == True:
         thread_ecoute.start()
         thread_enregistrer.start()
         thread_ecoute.join()
         thread_enregistrer.join()
 
     
-
+connects : bool = False
 def receive_data():
     while True:
         try:
             data = s.recv(1024)
-            receive_stream.write(data)
+            print(data)
+            if data[0:24] != b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00':
+                global connects
+                connects = False
+            else :
+                connects = True
+                receive_stream.write(data)
+
         except:
             pass
-
 
 def send_data():
     while True:
@@ -412,8 +421,8 @@ class ChatServer:
                                 time.sleep(0.001)
 
                         elif accept_appel == True and len(self.CONNECTION_LIST) > 2 and apparaitre == False:
-                            Fen_appel(self)
                             apparaitre = True
+                            Fen_appel(self)
 
 
                         elif accept_appel == True and len(self.CONNECTION_LIST) > 2 and apparaitre == True:
